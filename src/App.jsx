ce303d2c1f4598750eac3c1fcd72602fcb5524c6
adel123ben <adelformat541@gmail.com>
2025-06-25 16:51:13 +0100

    implementing the cooktails sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { gsap } from 'gsap';
 import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cooktails';
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +15,7 @@ export default function App() {
     <main>
       <Navbar />
       <Hero />
+      <Cocktails />
     </main>
   )
 }
